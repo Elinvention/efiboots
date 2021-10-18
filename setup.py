8b69ca6e2d2eb4336f65a985fcdfffb79f3af59b
Elia Argentieri <elia@elinvention.ovh>
2021-10-18 21:49:21 +0200

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='efibootmgr-gui',
+      version='1.0',
+      # Modules to import from other scripts:
+      packages=find_packages(),
+      # Executables
+      scripts=["efibootmgr_gui.py"],
+     )
