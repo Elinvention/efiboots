5b4e92f97483c77a1fd07fa676a1f5d472b80b19
Elia Argentieri <elia@elinvention.ovh>
2021-11-03 12:37:13 +0100

    Add a new test based on @SamLukeYes input #21

--- test/test_efiboots.py
+++ test/test_efiboots.py
@@ -93,3 +93,24 @@ class TestGUI(unittest.TestCase):
                 'timeout': 1
             }
             self.assertDictEqual(parsed, expected)
+
+        with open(test_dir / 'input5.test') as f:
+            parsed = efiboots.parse_efibootmgr(f)
+            expected = {
+                'entries': [
+                    {'num': '0000', 'active': True, 'name': 'SurfaceFrontPage', 'path': None, 'parameters': 'VOL+.'},
+                    {'num': '0001', 'active': True, 'name': 'Internal Storage', 'path': None, 'parameters': 'SDD.'},
+                    {'num': '0002', 'active': False, 'name': ' USB Storage', 'path': None, 'parameters': 'USB.'},
+                    {'num': '0003', 'active': True, 'name': 'PXE Network', 'path': None, 'parameters': 'PXE.'},
+                    {'num': '0004', 'active': False, 'name': ' linux-surface (reboot=pci)', 'path': None, 'parameters': 'root=LABEL=root initrd=intel-ucode.img initrd=initramfs-linux-surface.img zswap.enabled=0 reboot=pci'},
+                    {'num': '0005', 'active': True, 'name': 'linux-mainline', 'path': None, 'parameters': 'root=LABEL=root initrd=intel-ucode.img initrd=initramfs-linux-mainline.img zswap.enabled=0'},
+                    {'num': '0006', 'active': True, 'name': 'Linux Firmware Updater', 'path': None, 'parameters': ''},
+                    {'num': '000A', 'active': True, 'name': 'linux-zen', 'path': None, 'parameters': 'root=LABEL=root .initrd=intel-ucode.img initrd=initramfs-linux-zen.img zswap.enabled=0'},
+                    {'num': '000B', 'active': True, 'name': 'linux-surface', 'path': None, 'parameters': 'root=LABEL=root .initrd=intel-ucode.img initrd=initramfs-linux-surface.img zswap.enabled=0'}
+                ],
+                'boot_order': ['0002', '0004', '000B', '0005', '000A', '0003', '0001', '0006', '0000'],
+                'boot_next': None,
+                'boot_current': '000B',
+                'timeout': 0
+            }
+            self.assertDictEqual(parsed, expected)
