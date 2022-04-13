2ba1b238d0a086c300747889eb564dedef837f4b
Elia Argentieri <elia@elinvention.ovh>
2022-04-14 00:13:17 +0200

    Fix nix packaging

--- derivation.nix
+++ derivation.nix
@@ -1,10 +1,34 @@
-{ lib, gtk4, glib, gobject-introspection, efibootmgr, python3 }:
+{ lib, gtk4, glib, gobject-introspection, efibootmgr, python3, util-linux
+, wrapGAppsHook, desktop-file-utils, hicolor-icon-theme }:
 
+let
+  python_deps = with python3.pkgs; [
+    pygobject3
+  ];
+in
 python3.pkgs.buildPythonApplication rec {
   name = "efiboots";
   src = ./.;
-  buildInputs = [ gtk4 glib gobject-introspection ];
-  propagatedBuildInputs = [ efibootmgr ] ++ (with python3.pkgs; [ pygobject3 ]);
+
+  nativeBuildInputs = [
+    wrapGAppsHook
+    desktop-file-utils # needed for update-desktop-database
+    glib # needed for glib-compile-schemas
+    gobject-introspection # need for gtk namespace to be available
+    hicolor-icon-theme # needed for postinstall script
+    efibootmgr
+    util-linux
+  ];
+
+  buildInputs = [
+  	gtk4
+  	glib
+  ];
+
+  propagatedBuildInputs = python_deps;
+
+  doCheck = true;
+  checkInputs = buildInputs;
 
   meta = with lib; {
     description = " Manage EFI boot loader entries with this simple GUI";
@@ -12,5 +36,4 @@ python3.pkgs.buildPythonApplication rec {
     license = licenses.gpl3;
     platforms = platforms.linux;
   };
-  doCheck = true;
 }
