50d22411abb48880394845b66ae79cde5f482fa8
Elia Argentieri <elia@elinvention.ovh>
2023-06-11 12:37:55 +0200

    Improve nix support

---
+++ shell.nix
@@ -0,0 +1,43 @@
+with import <nixpkgs> { };
+
+let
+  pythonPackages = python3Packages;
+in pkgs.mkShell rec {
+  name = "impurePythonEnv";
+  venvDir = "./venv";
+  buildInputs = [
+    # A Python interpreter including the 'venv' module is required to bootstrap
+    # the environment.
+    pythonPackages.python
+
+    # This executes some shell code to initialize a venv in $venvDir before
+    # dropping into the shell
+    pythonPackages.venvShellHook
+
+    # Those are dependencies that we would like to use from nixpkgs, which will
+    # add them to PYTHONPATH and thus make them accessible from within the venv.
+    pythonPackages.numpy
+    pythonPackages.requests
+
+    # In this particular example, in order to compile any binary extensions they may
+    # require, the Python modules listed in the hypothetical requirements.txt need
+    # the following packages to be installed locally:
+    taglib
+    openssl
+    git
+    libxml2
+    libxslt
+    libzip
+    zlib
+
+    efibootmgr
+    util-linux
+
+    gobject-introspection
+    gtk4
+    glib
+    python3.pkgs.pygobject3
+
+    jetbrains.pycharm-professional
+  ];
+}
