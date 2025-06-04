5e020cd55be67dc0a487117c04ca7a5f0e7787a1
phanirithvij <phanirithvij2000@gmail.com>
2025-06-04 09:14:10 +0530

    nix: update derivation
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- flake.nix
+++ flake.nix
@@ -6,17 +6,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
       {
         devShells.default = import ./shell.nix { inherit pkgs; };
-
         packages = rec {
-          efiboots = pkgs.callPackage ./derivation.nix { inherit pkgs; };
+          efiboots = pkgs.callPackage ./derivation.nix { };
           default = efiboots;
         };
       }
     );
-
 }
